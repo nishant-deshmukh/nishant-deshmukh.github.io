a98c21982fab849841db35413981dd6caefe3ee5
Ragini Pandey <99394366+ragini-pandey@users.noreply.github.com>
2024-11-03 00:54:05 +0530

    Update index.js

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -25,7 +25,7 @@ const Navbar = () => {
     <Nav>
       <NavbarContainer>
         <NavLogo to="/">
-          <a style={{ display: "flex", alignItems: "center", color: "white", cursor: 'pointer' }}>
+          <a href="#" style={{ display: "flex", alignItems: "center", color: "white", cursor: 'pointer' }}>
             <DiCssdeck size="3rem" /> <Span>Portfolio</Span>
           </a>
         </NavLogo>
